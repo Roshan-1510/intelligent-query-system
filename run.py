9596b6eb32a8ee7bb4e72f664011b86c027712bf
Roshan Pramod Vishwakarma <roshanvishwakarma277@gmail.com>
2025-08-04 20:42:11 +0530

    Update run.py

--- run.py
+++ run.py
@@ -3,12 +3,11 @@ import uvicorn
 from config import settings
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    
+    port = int(os.environ["PORT"]) 
     uvicorn.run(
         "main:app",
         host="0.0.0.0",
         port=port,
-        reload=False,  # ✅ Always disable reload for production!
+        reload=settings.debug,
         log_level=settings.log_level.lower()
     )
