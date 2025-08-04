56a0891cd5c9e11a05ce4d1574235c2a8dcce0c5
Roshan Pramod Vishwakarma <roshanvishwakarma277@gmail.com>
2025-08-04 20:23:30 +0530

    Update run.py

--- run.py
+++ run.py
@@ -3,11 +3,12 @@ import uvicorn
 from config import settings
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))  # uses 8000 locally, dynamic PORT in prod
+    port = int(os.environ.get("PORT", 8000))
+    
     uvicorn.run(
         "main:app",
         host="0.0.0.0",
         port=port,
-        reload=True if settings.debug else False,
+        reload=False,  # ✅ Always disable reload for production!
         log_level=settings.log_level.lower()
     )
